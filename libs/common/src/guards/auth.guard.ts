984bda4b139c303c5986e1a5c9e0fca2fcc48df3
atul <atkr5496@gmail.com>
2022-10-24 00:01:28 +0530

    after authentication user data added to the header

--- libs/common/src/guards/auth.guard.ts
+++ libs/common/src/guards/auth.guard.ts
@@ -19,6 +19,7 @@ export class AuthGuard implements CanActivate {
         )
         .pipe(timeout(5000));
       const res$ = await lastValueFrom(source$);
+      req.user = res$;
       return res$;
     } catch (err) {
       Logger.error(err);
